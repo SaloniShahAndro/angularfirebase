138592d68eb8c33407898ed4c7f170f343c49de8
saloni <salonip1993@gmail.com>
2018-09-26 16:34:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
 
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>List of Data</h1>
+  <ul id="list">
+    <li *ngFor="let item of items | async" [ngClass]="{'active': selectedItem == item}" (click)="listClick($event, item)" >
+      {{ item.name }}
+    </li>
+
+  </ul>
+  <input type="text" id="message" placeholder="Type here..."
+  (keyup.enter)="chatSend($event.target.value)" [(ngModel)]="selectedItem" />
+<br/>
+  
+  `
 })
 export class AppComponent {
-  title = 'app';
+  items: Observable<any[]>;
+  itemRef;
+  itemRef1;
+  selectedItem;
+  newValue;
+  usr: Observable<any>;
+
+
+
+
+  constructor(public db: AngularFirestore, public af: AngularFireDatabase) {
+    //this.items = db.collection('users').valueChanges();
+    for (var i = 0; i < 15; i++) {
+      this.itemRef = this.af.object('items/' + i);
+      this.itemRef.set({ name: 'testing' + i, email: `testing${i}@gmail.com` });
+    }
+    this.itemRef.set({ name: 'saloni' })
+    this.items = af.list('items').valueChanges();
+
+  }
+
+  chatSend(message: string) {
+    /*  new node for users */
+    this.itemRef1 = this.af.object('users');
+    this.itemRef1.set({ name: message });
+    // this.itemRef.update({name: message},this.selectedItem)
+    
+    var category=this.af.list('/items', ref => 
+    ref.orderByChild('name').equalTo(this.selectedItem))
+    .valueChanges()
+    .subscribe(categoryItems => {
+      console.log(">>>selectedItem>>>",categoryItems);
+//return categoryItems
+
+      this.af.object(`items/2`).update({ name: message })
+
+    });
+   
+    console.log(">>>category>>>",category);
+
+    //this.af.list.push({'name':'user2'})
+  }
+
+  listClick(event, newValue) {
+    this.selectedItem = newValue.name;  // don't forget to update the model here
+    this.newValue = newValue;
+    //var newPostKey = firebase.database().ref().child('items').push().key;
+
+ 
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCqEym3XO6OCJoXqtTC1wex10o7CszUFw",
+    authDomain: "fcc-book-trading-c18ee.firebaseapp.com",
+    databaseURL: "https://fcc-book-trading-c18ee.firebaseio.com",
+    projectId: "fcc-book-trading-c18ee",
+    storageBucket: "fcc-book-trading-c18ee.appspot.com",
+    messagingSenderId: "102914441837"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule ,
+    AngularFireDatabaseModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
